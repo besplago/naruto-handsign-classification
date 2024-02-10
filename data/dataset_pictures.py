51436e93416ee72c0ac73b46f21c5535353b75b5
besplago <bruce_esplago@hotmail.com>
2024-02-10 21:51:53 +0100

    besplago buff pt 1 update: .gitignore refac: dataset_pictures.py

--- data/dataset_pictures.py
+++ data/dataset_pictures.py
@@ -1,52 +1,70 @@
-import cv2 as cv
+"""Create a dataset of pictures for each label in the data folder"""
 import os
-
+import sys
+import cv2 as cv
 from cv2 import waitKey
 
-#Sets to default camera
-camera = cv.VideoCapture(0)
+DATA_FOLDER: str = r"E:\Repos\naruto-handsign-classification\data"
+LABELS: list = [
+    "bird",
+    "boar",
+    "dog",
+    "dragon",
+    "hare",
+    "horse",
+    "monkey",
+    "ox",
+    "ram",
+    "rat",
+    "snake",
+    "tiger"
+]
+PICTURES_PER_LABEL: int = 50
 
-if not camera.isOpened():
-    print("Camera not opened, terminating..")
-    exit()
+def create_dataset() -> None:
+    """Create a dataset of pictures for each label in the data folder"""
+    if not os.path.exists(DATA_FOLDER):
+        os.mkdir(DATA_FOLDER)
 
-#Add/change/remove labels
-Labels = ["bird", "boar", "dog", "dragon", "Hare", "horse", "Monkey", "ox", "Ram", "Rat", "Snake", "Tiger"]
+    camera = cv.VideoCapture(0)
 
-for label in Labels:
-    if not os.path.exists(label):
-        os.mkdir(label)
+    if not camera.isOpened():
+        print("Camera not opened, terminating..")
+        sys.exit()
 
-for folder in Labels:
 
-    count = 0
+    for label in LABELS:
+        if not os.path.exists(label):
+            os.mkdir(f"{label}")
 
-    print("Press 'x' for "+folder)
-    userinput = input()
-    if userinput != 'x':
-        print("Wrong Input, terminating...")
-        exit()
+    for folder in LABELS:
+        print("Press 'x' for " + folder)
+        userinput = input()
+        if userinput != 'x':
+            print("Wrong Input, try again")
+            continue
 
-    #Delay before taking pictures
-    waitKey(2000)
-    
-    # Change number of pictures per label
-    while count<500:
+        waitKey(2000)
 
-        status, frame = camera.read()
+        count = 0
 
-        if not status:
-            print("Frame is not been captured, terminating...")
-            break
+        while count < PICTURES_PER_LABEL:
+            status, frame = camera.read()
 
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        cv.imshow("Video Window",gray)
-        gray = cv.resize(gray, (600,400))
-        cv.imwrite('D:/code/test/'+folder+'/img'+str(count)+'.png',gray)
-        count=count+1
-        waitKey(100)
-        if cv.waitKey(1) == ord('q'):
-            break
+            if not status:
+                print("Frame is not been captured, terminating...")
+                break
 
-camera.release()
-cv.destroyAllWindows()
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            cv.imshow("Video Window",gray)
+            gray = cv.resize(gray, (600,400))
+            cv.imwrite(f"{DATA_FOLDER}/{folder}/img{count}.png", gray)
+            count = count + 1
+            print(count)
+            waitKey(100)
+
+    camera.release()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    create_dataset()
