40695a4609dfb05b9d1e6193aabc4e54df2b58bd
besplago <bruce_esplago@hotmail.com>
2024-02-11 09:05:17 +0100

    refac: separate train and test data folders

--- data/dataset_pictures.py
+++ data/dataset_pictures.py
@@ -1,10 +1,12 @@
-"""Create a dataset of pictures for each label in the data folder"""
 import os
 import sys
 import cv2 as cv
 from cv2 import waitKey
+import shutil
 
 DATA_FOLDER: str = r"E:\Repos\naruto-handsign-classification\data"
+TRAIN_DATA_FOLDER: str = os.path.join(DATA_FOLDER, "train_data")
+TEST_DATA_FOLDER: str = os.path.join(DATA_FOLDER, "test_data")
 LABELS: list = [
     "bird",
     "boar",
@@ -20,11 +22,19 @@ LABELS: list = [
     "tiger"
 ]
 PICTURES_PER_LABEL: int = 50
+TEST_SPLIT: int = 20  # Percentage of data to be in the test set
+
 
 def create_dataset() -> None:
     """Create a dataset of pictures for each label in the data folder"""
     if not os.path.exists(DATA_FOLDER):
         os.mkdir(DATA_FOLDER)
+
+    if not os.path.exists(TRAIN_DATA_FOLDER):
+        os.makedirs(TRAIN_DATA_FOLDER)
+
+    if not os.path.exists(TEST_DATA_FOLDER):
+        os.makedirs(TEST_DATA_FOLDER)
 
     camera = cv.VideoCapture(0)
 
@@ -32,10 +42,11 @@ def create_dataset() -> None:
         print("Camera not opened, terminating..")
         sys.exit()
 
-
     for label in LABELS:
-        if not os.path.exists(label):
-            os.mkdir(f"{DATA_FOLDER}/{label}")
+        label_train_folder = os.path.join(TRAIN_DATA_FOLDER, label)
+        label_test_folder = os.path.join(TEST_DATA_FOLDER, label)
+        os.makedirs(label_train_folder, exist_ok=True)
+        os.makedirs(label_test_folder, exist_ok=True)
 
     for folder in LABELS:
         print("Press 'x' for " + folder)
@@ -56,15 +67,19 @@ def create_dataset() -> None:
                 break
 
             gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            cv.imshow("Video Window",gray)
-            gray = cv.resize(gray, (600,400))
-            cv.imwrite(f"{DATA_FOLDER}/{folder}/img{count}.png", gray)
+            cv.imshow("Video Window", gray)
+            gray = cv.resize(gray, (600, 400))
+            if count < PICTURES_PER_LABEL * (TEST_SPLIT / 100):
+                cv.imwrite(os.path.join(TEST_DATA_FOLDER, folder, f"img{count}.png"), gray)
+            else:
+                cv.imwrite(os.path.join(TRAIN_DATA_FOLDER, folder, f"img{count}.png"), gray)
             count = count + 1
             print(count)
             waitKey(100)
 
     camera.release()
     cv.destroyAllWindows()
+
 
 if __name__ == "__main__":
     create_dataset()
